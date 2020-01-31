29cf18a474ae12b5202052bef0cb2a77e708ed9b
Iurii_Kozak <Iurii_Kozak@epam.com>
2020-01-31 15:14:22 +0200

    fixes

--- src/main/java/debug/MySQLSelect.java
+++ src/main/java/debug/MySQLSelect.java
@@ -5,15 +5,44 @@ import mysql.*;
 import java.sql.*;
 
 public class MySQLSelect extends MySQLBase {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+//        selectMembers();
+        selectMails();
+    }
+
+    public static void selectMembers() {
         String query = MySQLQueries.selectFromQuery("members");
         ResultSet resultSet = new MySQLBase().executeQuery(query);
 
         StringBuilder stringBuilder = new StringBuilder();
-        while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String full_name = resultSet.getString("full_name");
-            stringBuilder.append(id).append(" ").append(full_name).append("\n");
+        try {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String full_name = resultSet.getString("full_name");
+                stringBuilder.append(id).append(" ").append(full_name).append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.print(stringBuilder.toString());
+    }
+
+    public static void selectMails() {
+        String query = MySQLQueries.selectFromQuery("mails");
+        ResultSet resultSet = new MySQLBase().executeQuery(query);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String subject = resultSet.getString("subject");
+                String email = resultSet.getString("email");
+                String body = resultSet.getString("body");
+                stringBuilder.append(id).append("\n").append(subject).append("\n")
+                        .append(email).append("\n").append(body).append("\n").append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         System.out.print(stringBuilder.toString());
     }
--- src/main/java/debug/MySQLUpdate.java
+++ src/main/java/debug/MySQLUpdate.java
@@ -5,9 +5,30 @@ import utils.Utils;
 
 public class MySQLUpdate extends MySQLBase {
     public static void main(String[] args) {
+//        updateMembers();
+        updateMails();
+    }
+
+    public static void updateMembers() {
         String fullName = new Utils().randomFullName;
         String query = MySQLQueries.updateQuery("members",
                 "full_name", fullName, "id", "1");
         new MySQLBase().executeUpdate(query);
+    }
+
+    public static void updateMails() {
+        String id = "1";
+        String subject = new Utils().randomSubject;
+        String email = new Utils().randomEmail;
+        String body = new Utils().randomBody;
+        String subjectQuery = MySQLQueries.updateQuery("mails",
+                "subject", subject, "id", id);
+        String emailQuery = MySQLQueries.updateQuery("mails",
+                "email", email, "id", id);
+        String bodyQuery = MySQLQueries.updateQuery("mails",
+                "body", body, "id", id);
+        new MySQLBase().executeUpdate(subjectQuery);
+        new MySQLBase().executeUpdate(emailQuery);
+        new MySQLBase().executeUpdate(bodyQuery);
     }
 }
